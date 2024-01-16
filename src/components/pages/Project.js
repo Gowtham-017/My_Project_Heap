8221022680a074aa87a9b723ea52507f56d73d4e
Gowtham K <113661092+Gowtham-017@users.noreply.github.com>
2024-01-16 21:49:18 +0530

    Update Project.js

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -16,19 +16,19 @@ import newyear from '../assets/newyear.png';
 import pythonandexcel from '../assets/pythonandexcel.png';
 const Project = () => {
     const [projectsData] = useState([
-        { imgsrc: portfolio, title: "Portfolio", description: "Description for Cookify", github: "https://github.com/Gowtham-017/Portfolio/", live: "https://gowthamk29.netlify.app/", },
-        { imgsrc: drummachine, title: "DrumMachine", description: "Description for Musify" , github: "https://github.com/Gowtham-017/DrumMachine", live: "https://www.google.com/",},
-        { imgsrc: newyear, title: "New-Year", description: "Description for Cookify" , github: "https://github.com/Gowtham-017/New_Year_2024", live: "https://www.google.com/",},
-        { imgsrc: lightbulb, title: "Light-Bulb", description: "Description for Cookify" , github: "https://github.com/Gowtham-017/Light_Bulb", live: "https://lightbulb17.netlify.app/",},
-        { imgsrc: mycompany, title: "My-Company", description: "Description for Cookify", github: "https://github.com/Gowtham-017/mycompany", live: "https://www.google.com/", },
-        { imgsrc: alarmclock, title: "Alarm-Clock", description: "Description for Cookify" , github: "https://github.com/Gowtham-017/Alarm_Clock", live: "https://alarm-clock-model.netlify.app/",},
-        { imgsrc: quotegenerator, title: "Random-Quotes", description: "Description for Cookify", github: "https://github.com/Gowtham-017/Random-Quotes", live: "https://quotesgen-random.netlify.app/", },
-        { imgsrc: kidsapp, title: "Kids-ColorApp", description: "Description for Musify" , github: "https://github.com/Gowtham-017/Color_App_Kids", live: "https://kids-color.netlify.app/",},
-        { imgsrc: playlistmanager, title: "PlayList-Manager", description: "Description for Musify" , github: "https://github.com/Gowtham-017/PlayList_Manager", live: "https://www.google.com/",},
-        { imgsrc: fakeproduct, title: "FakeProduct-Detector", description: "Description for Cookify" , github: "https://github.com/Gowtham-017/FakeProduct_Manager", live: "https://www.google.com/",},
-        { imgsrc: calculator, title: "Calculator", description: "Description for Musify", github: "https://github.com/Gowtham-017/Simple-Calculator", live: "https://easycalci.netlify.app/", },
-        { imgsrc: internshipmanager, title: "Internship-Manager", description: "Description for Cookify" , github: "https://github.com/Gowtham-017/Internship_Manager", live: "https://www.google.com/",},
-        { imgsrc: pythonandexcel, title: "Python-Excel", description: "Description for Cookify" , github: "https://github.com/Gowtham-017/Python_and_Excel", live: "https://www.google.com/",},
+        { imgsrc: portfolio, title: "Portfolio", description: "Description", github: "https://github.com/Gowtham-017/Personal_Website", live: "https://gowtham-dev.netlify.app/", },
+        { imgsrc: drummachine, title: "DrumMachine", description: "Description" , github: "https://github.com/Gowtham-017/DrumMachine", live: "https://www.google.com/",},
+        { imgsrc: newyear, title: "New-Year", description: "Description" , github: "https://github.com/Gowtham-017/New_Year_2024", live: "https://www.google.com/",},
+        { imgsrc: lightbulb, title: "Light-Bulb", description: "Description" , github: "https://github.com/Gowtham-017/Light_Bulb", live: "https://lightbulb17.netlify.app/",},
+        { imgsrc: mycompany, title: "My-Company", description: "Description", github: "https://github.com/Gowtham-017/mycompany", live: "https://www.google.com/", },
+        { imgsrc: alarmclock, title: "Alarm-Clock", description: "Description" , github: "https://github.com/Gowtham-017/Alarm_Clock", live: "https://alarm-clock-model.netlify.app/",},
+        { imgsrc: quotegenerator, title: "Random-Quotes", description: "Description", github: "https://github.com/Gowtham-017/Random-Quotes", live: "https://quotesgen-random.netlify.app/", },
+        { imgsrc: kidsapp, title: "Kids-ColorApp", description: "Description" , github: "https://github.com/Gowtham-017/Color_App_Kids", live: "https://kids-color.netlify.app/",},
+        { imgsrc: playlistmanager, title: "PlayList-Manager", description: "Description" , github: "https://github.com/Gowtham-017/PlayList_Manager", live: "https://www.google.com/",},
+        { imgsrc: fakeproduct, title: "FakeProduct-Detector", description: "Description" , github: "https://github.com/Gowtham-017/FakeProduct_Manager", live: "https://www.google.com/",},
+        { imgsrc: calculator, title: "Calculator", description: "Description", github: "https://github.com/Gowtham-017/Simple-Calculator", live: "https://easycalci.netlify.app/", },
+        { imgsrc: internshipmanager, title: "Internship-Manager", description: "Description" , github: "https://github.com/Gowtham-017/Internship_Manager", live: "https://www.google.com/",},
+        { imgsrc: pythonandexcel, title: "Python-Excel", description: "Description" , github: "https://github.com/Gowtham-017/Python_and_Excel", live: "https://www.google.com/",},
     ]);
     return (
         <div className='body'>
